a3a409fe443abf5a0da720094cc99c7e179651af
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-08 22:49:27 +0530

    debouncing implemented

--- debouncing/debounced.js
+++ debouncing/debounced.js
@@ -1,1 +1,22 @@
 //This function will return the debounced version of any provided function
+const debounce = function(fn, delay){
+    let timerId;
+    return function(...args){
+        clearTimeout(timerId);
+        timerId = setTimeout(()=>{
+            fn(...args)
+        }, delay);
+    }
+}
+
+const search = function(query){
+    console.log("Searching for : " + query);
+}
+
+const debouncedSearch = debounce(search, 100);
+
+debouncedSearch("H");
+debouncedSearch("HE");
+debouncedSearch("HEL");
+debouncedSearch("HELL");
+debouncedSearch("HELLO");
