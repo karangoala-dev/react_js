6c16f9300f106e554a538de4da7f0bc77e79b242
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-09 08:22:45 +0530

    commit msg

---
+++ debouncing/in_react/debouncing/src/App.jsx
@@ -0,0 +1,37 @@
+import {use, useCallback, useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[count, setCount] = useState(0);
+
+  //3-> This is a factory method which returns debounced version of any function
+  const debounced = (fn, delay) => {
+    let timerId;
+    return (...args) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    }
+  }
+
+  //2->This function will execute only when 1 second has passed since the previous keystroke and will print the latest state value only.
+  const debouncedButtonClick = useCallback(debounced((count) => {
+    console.log("Button clicked " + count + " times.");
+  }, 1000), []);
+
+  //1->This function will run the number of times button is clicked.
+  const handleClick = () => {
+    setCount(count + 1);
+    debouncedButtonClick(count);
+  }
+  return (
+    <>
+        <button onClick={() => handleClick()}>Debounced Button</button>
+    </>
+  )
+}
+
+export default App;
