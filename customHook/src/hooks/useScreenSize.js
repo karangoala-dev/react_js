57fa52adce72a9bf8b7e7792583502101de9ba79
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-22 23:50:39 +0530

    commit msg

---
+++ customHook/src/hooks/useScreenSize.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useScreenSize = function(){
+    const[size, setSize] = useState([0, 0]);
+
+    function updateScreenSize(){
+        
+    }
+
+    useEffect(()=>{
+
+    }, []);
+
+    return size;
+};
+
+export default useWidth;
