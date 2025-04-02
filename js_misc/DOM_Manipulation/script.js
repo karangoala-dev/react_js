afd2d2f18412c72e6ded0f188e294827d07d5ec2
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-02 20:11:06 +0530

    DOM

--- js_misc/DOM_Manipulation/script.js
+++ js_misc/DOM_Manipulation/script.js
@@ -12,9 +12,44 @@ console.log(heading);
 
 //changing inner html of an element DOM manipulation
 setTimeout(()=>{
-    heading.innerHTML = "Changed";
+    heading.innerHTML = "You will see a clock now";
     //or
     // heading.innerText = "New Text";   // Changes visible text
     // heading.textContent = "Text";  // Changes text without parsing HTML
 }, 2000);
 console.log(heading);
+
+//modifying attributes
+heading.setAttribute("class", "heading-class"); 
+heading.getAttribute("class");
+heading.removeAttribute("class");
+
+//modifying CSS
+setTimeout(()=>{
+    heading.style.color = "red";
+}, 5000);
+
+//creating and adding elements
+let clock = document.createElement("div");
+clock.setAttribute("id", "clock-div");
+document.body.appendChild(clock);
+let formatDate = function(i){
+    if(i < 10)
+        return "0" + i;
+    return i;
+}
+
+setTimeout(()=>{
+    let clockFn = function(){
+        let time = new Date();
+        let hr = formatDate(time.getHours());
+        let min = formatDate(time.getMinutes());
+        let sec = formatDate(time.getSeconds());
+        clock.innerText = `${hr}:${min}:${sec}`;
+        setTimeout(()=>{
+            clockFn();
+        }, 1000);
+    }
+    clockFn();
+}, 7000);
+
