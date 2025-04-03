f1af2c9c675df37b363e9165435cc84b5c576cd6
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-03 07:09:55 +0530

    DOM

--- react_hooks/src/components/UseEffectComponent.jsx
+++ react_hooks/src/components/UseEffectComponent.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { NavLink } from 'react-router-dom';
 import './UseEffectComponent.css';
 
 const UseEffectComponent = () => {
   const[data, setData] = useState([]);
+  const[page, setPage] = useState(1);
+  const[isBottom, setIsBottom] = useState(false);
+  //This variable refers to the bottom wala invisible div
+  const bottomRef = useRef(null);
   useEffect(()=>{
     const callApi = async ()=>{
       //Here both fetch and response.json return promises, so there are 2 promises here which need handling
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=15&_page=${page}`);
       const jsonData = await response.json();
-      setData(jsonData);
+      setData((prevData)=>[...prevData, ...jsonData]);
       //OR USE
       // await fetch('https://jsonplaceholder.typicode.com/posts').then(async (res)=>{
       //   const response = await res.json();
@@ -17,7 +21,25 @@ const UseEffectComponent = () => {
       // })
     }
     callApi();
-  }, [])
+  }, [page]);
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver((entries)=>{
+      //If bottom div is visible, we reached bottom
+      if(entries[0].isIntersecting == true){
+        setIsBottom(true);
+        setPage(page + 1);
+      }
+    }, {threshold: 1.0});
+
+    if(bottomRef.current != null){
+      observer.observe(bottomRef.current);
+    }
+
+    return ()=>{
+      observer.disconnect();
+    }
+  }, []);
 
   return (
     <div className='main'>
@@ -33,6 +55,8 @@ const UseEffectComponent = () => {
             </div>
           )
         })}
+      </div>
+      <div ref={bottomRef} >
       </div>
     </div>
   )
