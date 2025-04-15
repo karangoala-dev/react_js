9b398b61e5d2f793ae112e83d573123d6833ed81
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-15 08:36:13 +0530

    first commit

--- passwordField/src/App.jsx
+++ passwordField/src/App.jsx
@@ -3,17 +3,17 @@ import { Eye, EyeOff } from 'lucide-react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[visible, setVisible] = useState(false);
 
   return (
     <>
       <label>Password show / hide functionality shown here</label>
-      <div>
-        <input></input>
-        <button></button>
+      <div className='password-main'>
+        <input className='password-input' type={visible ? 'text': 'password'}></input>
+        <button className='password-btn' onClick={()=>setVisible(!visible)}>{visible ? 'Show' : 'Hide'}</button>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
