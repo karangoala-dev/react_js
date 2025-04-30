e1021e967b8735023dcfabfa03f5b74684173ed7
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-30 21:30:01 +0530

    fileExplorer

--- fileExplorer/src/List.jsx
+++ fileExplorer/src/List.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 const List = ({list}) => {
+    const[expanded, setExpanded] = useState({});
     return(
         <div className='container'>
             {list.map((node)=>{
                 return(
                 <div key={node.id}>
-                    <span>{node.name}</span>
+                    <span>{node.children.length != 0 && <span onClick={() => setExpanded((prev)=>({...prev, [node.name]: !prev[node.name]}))}>{expanded[node.name] ? '- ' : '+ '}</span>}{node.name}</span>
                     {/* recursive step */}
-                    {node && node.children.length != 0 && <List list={node.children}/>}
+                    {expanded[node.name] && node && node.children.length != 0 && <List list={node.children}/>}
                 </div>
                 )
             })}
