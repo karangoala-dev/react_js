641ef210cef9904fd139ac68104964b304b8ee94
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-01 07:57:43 +0530

    fileExplorer

--- fileExplorer/src/components/App.jsx
+++ fileExplorer/src/components/App.jsx
@@ -9,20 +9,22 @@ function App() {
   //this fn traverses the folder tree and adds a folder inside parentId
   const addFolder = (parentId) => {
     const name = prompt('Enter folder name ');
-    //this is a fn which is called recursively
+    // this is a fn which is called recursively
     const updateTree = (list) => {
+      // this returns the list after adding the new folder to the children of parent if found
       return list.map((node)=>{
         if(node.id == parentId){
           return {
             ...node,
             children: [...node.children, {
-              id: Date.now.toString(),
+              id: Date.now().toString(),
               name: name,
               isFolder: true,
               children: []
             }]
           }
         }
+        // if parent is not found, then call same fn on it's children if it exists
         if(node.children){
           return {...node, children: updateTree(node.children)};
         }
@@ -32,8 +34,25 @@ function App() {
     setData((prev)=>updateTree(prev));
   }
 
+  //this fn deletes the file node from data tree
   const deleteFolder = (id) => {
-    
+
+    //this fn is called recursively
+    const updateTree = (list) => {
+
+      //first filter out all items where id = given id
+      //then again run a .map on each filtered item for recursively calling the updateTree fn on it's children
+      return list.filter((item)=>item.id != id).map((item)=>{
+        if(item.children){
+          return {
+            ...item,
+            children: updateTree(item.children)
+          }
+        }
+      })
+    }
+
+    setData((prev) => updateTree(prev));
   }
 
   return (
