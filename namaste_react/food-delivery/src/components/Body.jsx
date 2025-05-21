b919c3e40637403f17d135bc42b7678d45bca70a
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-21 21:59:06 +0530

    namaste-react

--- namaste_react/food-delivery/src/components/Body.jsx
+++ namaste_react/food-delivery/src/components/Body.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { use, useState } from "react";
+import { useEffect, useState } from "react";
 import json from '../data.json';
 import Restaurant from './Restaurant';
 
 const Body = () => {
     const[searchKey, setSearchKey] = useState("");
     const[data, setData] = useState(json);
+
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const promise = await fetch('https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9715987&lng=77.5945627&str=Biryani&trackingId=4b1f8833-85ab-ba8b-069a-82535f1a7b46&submitAction=ENTER&queryUniqueId=56eac245-bd21-54c4-c0ef-cd0c908082d7');
+        const response = await promise.json();
+        console.log(response);
+    }
 
     const useTopRated = function(){
         setData(data.filter((item) => item.rating > 4.0));
