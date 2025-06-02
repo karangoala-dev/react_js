921b35e32e7e0bfbb51a2a1940ed4ce80f6191d7
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-02 14:53:00 +0530

    namaste-react

--- otp_input/src/App.jsx
+++ otp_input/src/App.jsx
@@ -8,6 +8,16 @@ function App() {
   //initialise an empty array with blank value
   const[inputArr, setInputArr] = useState(new Array(OTP_DIGITS).fill(""));
 
+  const handleChange = (value, index) => {
+    if(isNaN(value)){
+      return;
+    }
+    const newValue = value.trim();
+    const dummyArr = [...inputArr];
+    dummyArr[index] = newValue;
+    setInputArr(dummyArr);
+  }
+
   return (
     <div className='main'>
       <h1 className='header'>
@@ -15,7 +25,7 @@ function App() {
       </h1>
       <div className='otp-secn'>
         {
-          inputArr.map((item, i) => <input className='digit' value={inputArr[i]} />)
+          inputArr.map((item, i) => <input key={i} className='digit' value={inputArr[i]} onChange={(e)=>handleChange(e.target.value, i)}/>)
         }
       </div>
     </div>
