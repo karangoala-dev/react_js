9a281fcef31b2ad5e44e61349c7a20cba13bf36d
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-03 13:26:50 +0530

    namaste-react

--- otp_input/src/App.jsx
+++ otp_input/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import './App.css'
+import { useEffect } from 'react';
 
 
 const OTP_DIGITS = 6;
@@ -9,6 +10,10 @@ function App() {
   const[inputArr, setInputArr] = useState(new Array(OTP_DIGITS).fill(""));
   const refArr = useRef([]);
 
+  useEffect(()=>{
+    refArr.current[0]?.focus();
+  }, []);
+
   const handleChange = (value, index) => {
     if(isNaN(value)){
       return;
@@ -17,6 +22,18 @@ function App() {
     const dummyArr = [...inputArr];
     dummyArr[index] = newValue;
     setInputArr(dummyArr);
+    //go to next input only if value is not present
+    newValue && refArr.current[index + 1]?.focus();
+  }
+
+  const handleOnKeyDown = (e, index) => {
+    //e holds the key name
+    //the condition !e.target.value is needed since this fn must focus to previous input box only if current data is empty
+    //if not empty, no need to focus as this onKeyDown triggers before the input value changes.
+    //so before delete happens, it is executed and shifts focus to previous and previous gets deleted
+    if(!e.target.value && e.key === "Backspace"){
+      refArr.current[index - 1]?.focus();
+    }
   }
 
   return (
@@ -31,6 +48,7 @@ function App() {
                   className='digit' 
                   value={inputArr[i]} 
                   ref = {(input) => (refArr.current[i] = input)}
+                  onKeyDown = {(e)=>handleOnKeyDown(e, i)}
                   onChange={(e)=>handleChange(e.target.value, i)}/>)
         }
       </div>
